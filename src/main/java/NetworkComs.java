eb5be7acf1b7e3af3a46e6af4ef8f62ac02df608
hjhornbeck <hjhornbeck@protonmail.com>
2021-05-25 11:45:04 -0600

    - Switched the project to use the local JDK, OpenJDK 1.8. - Started to expand on NetworkComs.

--- src/main/java/NetworkComs.java
+++ src/main/java/NetworkComs.java
@@ -8,12 +8,40 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import javax.net.ssl.SSLContext;
-import tlschannel.ClientTlsChannel;
-import tlschannel.TlsChannel;
+import tlschannel.*;
+
 
 /**
- *
+ * Handles network communication for SeedPublisher.
  */
-
 public class NetworkComs {
+
+    /**
+     * The foreign host to connect to.
+     */
+    private String host;
+
+    /**
+     * The foreign port to connect to.
+     */
+    private int port;
+
+    /**
+     * The API key used to identify the current user.
+     */
+    private String api_key;
+
+    /**
+     * Initialize this class. No connections will be made until the member methods are called.
+     * @param host The hostname to connect to.
+     * @param port The port to connect to at that hostname.
+     * @param api_key The API key to pass, uniquely identifying the current user.
+     */
+    public NetworkComs( String host, int port, String api_key ) {
+
+        this.host = host;
+        this.port = port;
+        this.api_key = api_key;
+
+    }
 }
