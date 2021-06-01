cf975f3642d64ae112e2f0a3d228bb0662442ac9
hjhornbeck <hjhornbeck@protonmail.com>
2021-05-31 22:46:26 -0600

    - Removed tls-channel, as I suspect Gradle is now handling it. - Expanded SeedStorage a bit.

--- src/main/java/SeedStorage.java
+++ src/main/java/SeedStorage.java
@@ -1,4 +1,16 @@
+import java.util.LinkedList;
+
+/**
+ * A container for discovered seeds. This allows for flexibility within the hunt;
+ *  multiple hunts can be run simultaneously with different settings with the results
+ *  being fed into multiple SeedStorage containers. It also makes writing the hunt code
+ *  a bit easier, as it need not worry about locking or storage.
+*/
 public class SeedStorage {
 
+    /**
+     * Store
+     */
+    private LinkedList queue;
 
 }
